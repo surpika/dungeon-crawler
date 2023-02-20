1ce2b33945cd797965840b177bb0be9def30857d
Lawrence Ng <surpika2020@gmail.com>
2023-02-19 17:01:51 -0800

    first commit

---
+++ W23-Assignment-3/proc-gen.js
@@ -0,0 +1,56 @@
+const wall = 1;
+const empty = 0;
+
+const Proc_Gen =
+	class Proc_Gen {
+		constructor() {
+			this.map = [];
+			this.tiles = [];
+			this.generate_map();
+			console.log("testing");
+		}
+		
+		generate_map() {
+			for (let i = 0; i < 15; i++) {
+				let row = [];
+				for (let j = 0; j < 15; j++) {
+					row.push(Math.round(Math.random()));
+				}
+				this.map.push(row);
+			}
+			for(let i = 6; i < 9; i++) {
+				for(let j = 6; j < 9; j++) {
+					this.map[i][j] = empty
+				}
+			}
+			
+			for(let i = 0; i < 15; i++) {
+				let row = [];
+				for (let j = 0; j < 15; j++) {
+					row.push('1111'); //represents solid wall; render nothing.
+				}
+				this.tiles.push(row);
+			}
+			
+			console.log([0, 1, 0, 1].join('') === '0101');
+			for (let i = 3; i < 12; i++) {
+				for(let j = 3; j < 12; j++) {
+					if(this.map[i][j] == empty) {
+						this.tiles[i][j] = 
+						[
+							+(this.map[i-1][j] == wall),
+							+(this.map[i][j-1] == wall),
+							+(this.map[i+1][j] == wall),
+							+(this.map[i][j+1] == wall)
+						].join(''); //write array in string format, e.g. '0101'
+					}
+				}
+			}
+			console.log(this.map);
+			console.log(this.tiles);
+			
+			
+		}
+	}
+	
+export default Proc_Gen
